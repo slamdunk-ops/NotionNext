2107fc667599ceb12c96499e834530ddce16bfba
slamdunk-ops <1742257343@qq.com>
2024-07-07 13:19:32 +0800

    1

--- themes/simple/components/MenuList.js
+++ themes/simple/components/MenuList.js
@@ -56,7 +56,7 @@ export const MenuList = ({ customNav, customMenu }) => {
   ]
 
   if (customNav) {
-    links = links.concat(customNav)
+    //links = links.concat(customNav)
   }
 
   // 如果 开启自定义菜单，则覆盖Page生成的菜单
